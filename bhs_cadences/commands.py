90efdaeb75ed835eab36c44f4af2eb217cfe3a69
Aaron Hoffman <aaron_hoffman@fastmail.com>
2024-04-10 20:10:43 -0700

    progressed add-metadata

--- bhs_cadences/commands.py
+++ bhs_cadences/commands.py
@@ -1,13 +1,37 @@
 import click
 from flask import current_app as app
 from flask import Blueprint
+from dataclasses import dataclass
+import json
+import os
 
 bp = Blueprint("commands", __name__)
 
+@dataclass
+class Metadata:
+    title: str
+    description: str = ""
+    author: str = ""
+    date_added: str = ""
+
+    
+
+
 @bp.cli.command("add-metadata")
 @click.argument("score")
-def add_metadata(score):
-    print(score)
+@click.option('--title', help='score title')
+@click.option('--description', help='score description')
+@click.option('--author', help='score\'s author')
+@click.option('--date-added', help='score added to BHS cadence list (yyyy-mm-dd)')
+def add_metadata(score, title, description=None, author=None, date_added=None):
+    # print(score)
+    data = Metadata(title = title, description = description, author = author, date_added = date_added)
+    # print(data)
+    data_dict = data.__dict__
+    # print(data_dict)
+    path = "/Users/xallax/Documents/python/bhs_cadences/data/" + score + "/metadata.json"
+    with open(path, "w") as outfile:
+        json.dump(data_dict, outfile, indent=2) 
 
 @bp.cli.command("list-scores")
 def list_scores():
