5c7d303efe228b29dc685beeafa29c4b35baf7fa
Aaron Hoffman <aaron_hoffman@fastmail.com>
2024-04-10 20:28:37 -0700

    changed add-metadata to create-score, which now makes metadata.json and instrument folders

--- bhs_cadences/commands.py
+++ bhs_cadences/commands.py
@@ -1,11 +1,14 @@
+import json
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
 import click
 from flask import current_app as app
 from flask import Blueprint
-from dataclasses import dataclass
-import json
-import os
 
 bp = Blueprint("commands", __name__)
+INSTRUMENTS = ["snare", "tenors", "cymbals", "bass"]
 
 @dataclass
 class Metadata:
@@ -15,21 +18,24 @@ class Metadata:
     date_added: str = ""
 
     
-
-
-@bp.cli.command("add-metadata")
+@bp.cli.command("create-score")
 @click.argument("score")
 @click.option('--title', help='score title')
 @click.option('--description', help='score description')
 @click.option('--author', help='score\'s author')
 @click.option('--date-added', help='score added to BHS cadence list (yyyy-mm-dd)')
-def add_metadata(score, title, description=None, author=None, date_added=None):
-    # print(score)
+def create_score(score, title, description=None, author=None, date_added=None):
+    """Add metadata to score. DATA_DIR must define the absolute path to the data directory"""
+    data_dir = Path(app.config['DATA_DIR']) / Path(score)
+    data_dir.mkdir(parents=True, exist_ok=True)
+    for instrument in INSTRUMENTS:
+        (data_dir / Path(instrument)).mkdir(parents=True, exist_ok=True)
+    assert data_dir.is_dir(), f'{data_dir} is not a valid directory'
+
     data = Metadata(title = title, description = description, author = author, date_added = date_added)
-    # print(data)
     data_dict = data.__dict__
-    # print(data_dict)
-    path = "/Users/xallax/Documents/python/bhs_cadences/data/" + score + "/metadata.json"
+    
+    path = data_dir / Path("metadata.json")
     with open(path, "w") as outfile:
         json.dump(data_dict, outfile, indent=2) 
 
