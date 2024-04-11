b768b2bd0ba707dd33f4db26825dc89406c21ff1
Aaron Hoffman <aaron_hoffman@fastmail.com>
2024-04-10 21:12:13 -0700

    create utils.py and modules.py

--- bhs_cadences/commands.py
+++ bhs_cadences/commands.py
@@ -7,54 +7,10 @@ import click
 from flask import current_app as app
 from flask import Blueprint
 
+from bhs_cadences.models import Score, Instrument, Metadata, INSTRUMENTS
+from bhs_cadences.utils import get_scores
+
 bp = Blueprint("commands", __name__)
-INSTRUMENTS = ["snare", "tenors", "cymbals", "bass"]
-
-@dataclass
-class Metadata:
-    title: str
-    description: str = ""
-    author: str = ""
-    date_added: str = ""
-
-@dataclass
-class Instrument:
-    instrument: str
-    audio: str #representing path
-    pdf: str
-    muse_file: str
-    
-@dataclass
-class Score:
-    metadata: Metadata
-    instruments: list[Instrument]
-
-def get_scores(data_dir: Path):
-    scores = []
-    for folder in data_dir.iterdir():
-        metadata = folder / Path("metadata.json")
-        if not metadata.is_file():
-            continue
-
-        with open(metadata, "r") as f:
-            data = json.load(f)
-            score = Score(metadata=Metadata(**data), instruments=[])
-
-        for instrument in INSTRUMENTS:
-            audio = folder / Path(instrument) / Path("audio.mp3")
-            pdf = folder / Path(instrument) / Path("score.pdf")
-            muse_file = folder / Path(instrument) / Path("muse_file.mscz")
-            score.instruments.append(
-                Instrument(
-                    instrument=instrument, 
-                    audio=audio if audio.is_file() else None, 
-                    pdf=pdf if pdf.is_file() else None, 
-                    muse_file=muse_file if muse_file.is_file() else None)
-                    )
-
-        scores.append(score)
-
-    return scores
     
 @bp.cli.command("create-score")
 @click.argument("score")
---
+++ bhs_cadences/models.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+INSTRUMENTS = ["snare", "tenors", "cymbals", "bass"]
+
+@dataclass
+class Metadata:
+    title: str
+    description: str = ""
+    author: str = ""
+    date_added: str = ""
+
+@dataclass
+class Instrument:
+    instrument: str
+    audio: str #representing path
+    pdf: str
+    muse_file: str
+    
+@dataclass
+class Score:
+    metadata: Metadata
+    instruments: list[Instrument]
