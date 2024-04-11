a5cb7f83fb04c609ccfb4930dbf1750d3b8513c2
Aaron Hoffman <aaron_hoffman@fastmail.com>
2024-04-11 11:40:52 -0700

    changed scores to g.scores

--- bhs_cadences/views.py
+++ bhs_cadences/views.py
@@ -1,7 +1,7 @@
 import pprint
 from pathlib import Path
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, g
 from flask import current_app as app
 from flask import render_template
 
@@ -9,22 +9,22 @@ from bhs_cadences.utils import get_scores
 
 bp = Blueprint("root", __name__)
 
+@bp.before_request
+def before_request():
+    g.scores = get_scores(Path(app.config['DATA_DIR']))
 
 @bp.route("/")
 def index():
     app.logger.warning("loading index.html")
-    scores = get_scores(Path(app.config['DATA_DIR']))
-    print(scores)
     return render_template(
         "index.html", 
-        scores=scores)
+        scores=g.scores)
 
 @bp.route("/mp3/<score>/<instrument>")
 def mp3(score, instrument):
     # list scores and find the path to the 
     # audio file for this score and instrument
-    scores = get_scores(Path(app.config['DATA_DIR']))
-    filepath = scores[score].instruments[instrument].audio
+    filepath = g.scores[score].instruments[instrument].audio
 
     # def generate():
     #     with open(filepath, "rb") as fwav:
@@ -42,8 +42,7 @@ def mp3(score, instrument):
 def pdf(score, instrument):
     # list scores and find the path to the 
     # audio file for this score and instrument
-    scores = get_scores(Path(app.config['DATA_DIR']))
-    filepath = scores[score].instruments[instrument].pdf
+    filepath = g.scores[score].instruments[instrument].pdf
     with open(filepath, "rb") as fpdf:
         return Response(fpdf.read(), mimetype="application/pdf")
 
@@ -51,8 +50,7 @@ def pdf(score, instrument):
 def mscz(score, instrument):
     # list scores and find the path to the 
     # audio file for this score and instrument
-    scores = get_scores(Path(app.config['DATA_DIR']))
-    filepath = scores[score].instruments[instrument].muse_file
+    filepath = g.scores[score].instruments[instrument].muse_file
     with open(filepath, "rb") as fmuse:
         return Response(fmuse.read(), mimetype="application/x-musescore")
 
